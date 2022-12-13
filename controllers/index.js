1f010f208ea2cb8aa9ab8fab4c0a9b513c3d0a81
Ryan Z <ryanzade@hotmail.com>
2022-12-13 21:33:34 +1030

    updated controllers file

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-const favouriteRoutes = require('./home-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('./favourite', homeRoutes);
-router.use('./recentreviews', homeRoutes);
 
 module.exports = router;
