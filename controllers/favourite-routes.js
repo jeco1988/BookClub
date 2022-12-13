d6e696ae734e34914edbb37268fe6fb1d536798c
Ryan Z <ryanzade@hotmail.com>
2022-12-13 20:32:20 +1030

    favourite route updated

--- controllers/favourite-routes.js
+++ controllers/favourite-routes.js
@@ -1,24 +1,24 @@
 const router = require('express').Router();
 const Favourite = require('../models/Favourite');
 
-router.get('/myFavourites', async (req, res) => {
-    try {
-      const favouriteData = await Favourite.findAll({
-        include: [
-          {
-            model: Favourite,
-            attributes: ['must_read'],
-          }],
-      });
+// router.get('/myFavourites', async (req, res) => {
+//     try {
+//       const favouriteData = await Favourite.findAll({
+//         include: [
+//           {
+//             model: Favourite,
+//             attributes: ['must_read'],
+//           }],
+//       });
 
-      const favourites = favouriteData.map((book) => book.get({ plain: true }));
-      res.render('myfavourites', { 
-        favourites,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+//       const favourites = favouriteData.map((book) => book.get({ plain: true }));
+//       res.render('myfavourites', { 
+//         favourites,
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
   router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
