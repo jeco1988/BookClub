b9480e3a8d8acba05d7548a70de05454b26d60ae
Jasmin Shea <jasminsh675@gmail.com>
2022-12-13 19:06:47 +1000

    model seeding

---
+++ seeds/favouriteseed.js
@@ -0,0 +1,21 @@
+const { Favourite } = require('../models');
+
+const favourites = [
+    {
+      user_id: 1,
+      book_id: 1
+    },
+    {
+      user_id: 1,
+      book_id: 2
+    },
+    {
+      user_id: 2,
+      book_id: 3
+    }
+  ];
+  
+  const seedFavourite = () => Favourite.bulkCreate(favourites);
+
+  module.exports = seedFavourite;
+  
