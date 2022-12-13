8863309b00fa82cfc7d044f883d551d9803b060f
Ryan Z <ryanzade@hotmail.com>
2022-12-13 20:33:18 +1030

    home  route updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const Book = require('../models/Book');
+const Review = require('../models/Review');
+const Favourite = require('../models/Favourite');
 
 router.get('/', async (req, res) => {
     try {
@@ -12,13 +14,52 @@ router.get('/', async (req, res) => {
       });
 
       const books = bookData.map((book) => book.get({ plain: true }));
-      res.render('//homepage//??', { 
+      res.render('mustread', { 
         books,
       });
     } catch (err) {
       res.status(500).json(err);
     }
   });
+
+  router.get('/recentreviews', async (req, res) => {
+    try {
+      const reviewsData = await Review.findAll({
+        include: [
+          {
+            model: Review,
+            attributes: ['review_date'],
+            order: [['review_date', 'ASC']],
+          }],
+      });
+
+      const reviews = reviewsData.map((review) => review.get({ plain: true }));
+      res.render('recentreviews', { 
+        reviews,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/favourites', async (req, res) => {
+        try {
+          const favouriteData = await Favourite.findAll({
+            include: [
+              {
+                model: Favourite,
+                attributes: ['book_id'],
+              }],
+          });
+    
+          const favourites = favouriteData.map((favourite) => favourite.get({ plain: true }));
+          res.render('favourites', { 
+            favourites,
+          });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      });
 
   router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
