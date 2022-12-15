7a6a6d3cd391139869f2b93c2956a025602ae2a6
Jasmin Shea <jasminsh675@gmail.com>
2022-12-15 20:22:33 +1000

    seed reorder

--- public/js/login.js
+++ public/js/login.js
@@ -4,11 +4,8 @@ const loginFormHandler = async (event) => {
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
+      const response = await axios.post('/api/users/login', {
+        data: { email, password }
       });
   
       if (response.ok) {
@@ -16,8 +13,7 @@ const loginFormHandler = async (event) => {
       } else {
         alert('Failed to log in');
       }
-    }
-  };
+    };
   
   document
     .querySelector('.login-form')
